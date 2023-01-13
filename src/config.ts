1131346783f18d8b6a58187154366d7c30dd05f9
Novo <eunovo9@gmail.com>
2023-01-13 01:31:50 +0100

    Setup config file

---
+++ src/config.ts
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import { EnvValue } from "./utils";
+
+dotenv.config();
+
+class Config {
+
+    @EnvValue({
+        name: 'BITCOIND_RPC_USERNAME',
+        isRequired: true
+    })
+    BITCOIND_RPC_USERNAME!: string;
+    
+    @EnvValue({
+        name: 'BITCOIND_RPC_PASSWORD',
+        isRequired: true
+    })
+    BITCOIND_RPC_PASSWORD!: string;
+    
+    @EnvValue({
+        name: 'BITCOIND_HOST',
+        default: '127.0.0.1'
+    })
+    BITCOIND_HOST!: string;
+
+    @EnvValue({
+        name: 'BITCOIND_PORT',
+        default: 8332
+    })
+    BITCOIND_PORT!: number;
+
+}
+
+export const config = new Config();
